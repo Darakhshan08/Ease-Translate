e3321c0de96e2200d82be0ed9294b8d8f5e52ab9
RayyanAhmed009 <158506139+RayyanAhmed009@users.noreply.github.com>
2024-09-26 01:52:07 +0500

    check kro
    check kro

--- translate_ease/lib/app/modules/home/views/home_view.dart
+++ translate_ease/lib/app/modules/home/views/home_view.dart
@@ -10,8 +10,6 @@ import '../controllers/home_controller.dart';
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
 
-
-
   void closeAppUsingExit() {
     exit(0);
   }
@@ -20,76 +18,49 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     controller.setTimer();
     return SafeArea(
-      child: WillPopScope(
-        onWillPop: () async {
-          // Show an alert dialog when the user presses the back button
-          bool exit = await showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text('Exit App?'),
-              content: Text('Are you sure you want to exit the app?'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(false);
-                  },
-                  child: Text('Cancel'),
+        child: WillPopScope(
+            onWillPop: () async {
+              // Show an alert dialog when the user presses the back button
+              bool exit = await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text('Exit App?'),
+                  content: Text('Are you sure you want to exit the app?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                      child: Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        closeAppUsingExit();
+                      },
+                      child: Text('Exit'),
+                    ),
+                  ],
                 ),
-                TextButton(
-                  onPressed: () {
-                    closeAppUsingExit();
-                  },
-                  child: Text('Exit'),
-                ),
-              ],
-            ),
-          );
-          return exit ?? false;
-        },
-        child: Scaffold(
-            body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/layer1.png',
-                fit: BoxFit.cover,
-                height: 200,
-                width: double.infinity,
-              ),
-              Spacer(),
-              Image.asset(
-                'assets/background.png',
-                height: 100,
-                width: 70,
-              ),
-              // const Text(
-              //   "Translate Ease",
-              //   style: TextStyle(
-              //     fontSize: 24,
-              //     color: Colors.blue,
-              //     fontWeight: FontWeight.bold,
-              //   ),
-              // ),
-              Spacer(),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: SizedBox(
-                  width: 70,
-                  child: LoadingIndicator(
-                    indicatorType: Indicator.ballRotateChase,
-                    colors: [Colors.blue],
-                    strokeWidth: 2,
-                    backgroundColor: Colors.transparent,
-                    pathBackgroundColor: Colors.black,
+              );
+              return exit ?? false;
+            },
+            child: Scaffold(
+              body: Container(
+                color: Colors.black, // Set the background color to black
+                child: const Center(
+                  // Center the LoadingIndicator
+                  child: SizedBox(
+                    width: 100,
+                    child: LoadingIndicator(
+                      indicatorType: Indicator.ballRotateChase,
+                      colors: [Colors.blue],
+                      strokeWidth: 2,
+                      backgroundColor: Colors.transparent,
+                      pathBackgroundColor: Colors.black,
+                    ),
                   ),
                 ),
-              )
-            ],
-          ),
-        )),
-      ),
-    );
+              ),
+            )));
   }
 }
